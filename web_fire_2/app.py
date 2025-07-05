fc9937520897922c2e7ad6e914083fa63a3874bf
maidongthuc <chechanh2003@gmail.com>
2025-07-05 08:58:50 +0700

    5050

--- web_fire_2/app.py
+++ web_fire_2/app.py
@@ -190,4 +190,4 @@ def get_image(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5050)
